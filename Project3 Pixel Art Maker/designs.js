bcc711f6a99136ddad0a8eecfb0b42532029d668
shalantor <protipas2@hotmail.com>
2018-01-13 13:38:05 +0200

    Project3: Use keyword let for variables

--- Project3 Pixel Art Maker/designs.js
+++ Project3 Pixel Art Maker/designs.js
@@ -10,26 +10,26 @@ $("#sizePicker").submit(function(event){
 // When a table cell is clicked, change its background color
 // Have to use on function because child is dynamically created content
 $("#pixel_canvas").on("click", "td", function() {
-  var color = $("#colorPicker").val();
+  let color = $("#colorPicker").val();
   $(this).attr("bgcolor", color);
 });
 
 function makeGrid() {
 
   //Get values from input fields
-  var height = $("#input_height").val();
-  var width = $("#input_width").val();
+  let height = $("#input_height").val();
+  let width = $("#input_width").val();
 
   //Get table to add rows and cells
-  var table = $("#pixel_canvas");
+  let table = $("#pixel_canvas");
 
   //First remove the previous grid
   table.children().remove();
 
   //Add table cells and rows equal to width and height
-  for(var i = 0; i < height; i++ ){
+  for(let i = 0; i < height; i++ ){
     table.append("<tr></tr>");
-    for(var j = 0; j < width; j++ ){
+    for(let j = 0; j < width; j++ ){
       table.find("tr").last().append("<td></td>");
     }
   }
