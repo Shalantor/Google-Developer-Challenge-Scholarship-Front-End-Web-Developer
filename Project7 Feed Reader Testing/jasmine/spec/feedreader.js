d85808e4cea7e15d51362b13a5afd3c7fdde599b
George <protipas2@hotmail.com>
2018-05-19 20:13:57 +0300

    Project 7 test: Check number of entries after load

--- Project7 Feed Reader Testing/jasmine/spec/feedreader.js
+++ Project7 Feed Reader Testing/jasmine/spec/feedreader.js
@@ -90,6 +90,18 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done){
+            loadFeed(0,function(){
+                done();
+            });
+         });
+
+         it('has at least one entry',function(done){
+            let container = document.querySelector('.feed');
+            let entries = container.getElementsByClassName('entry');
+            expect(entries.length).toBeGreaterThan(0);
+            done();
+         });
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
