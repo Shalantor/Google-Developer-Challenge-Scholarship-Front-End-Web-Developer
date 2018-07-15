b83dfda782f511733fa8510d10a198a60bc3f985
George <protipas2@hotmail.com>
2018-07-15 18:14:33 +0300

    Feat: Add map box token in restaurant page

--- Project8 Restaurant Reviews App Stage 1/js/restaurant_info.js
+++ Project8 Restaurant Reviews App Stage 1/js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZ2VvcmdrYXJhb2xhbmlzMTIzIiwiYSI6ImNqaXg4NGZoNDMyOGwza3J6NnRmZzFkOGYifQ.2aziL54wxRTKMCT1rzqTyQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
