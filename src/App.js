a7eaa685731e67103a5f2e967c570905f8993947
George <protipas2@hotmail.com>
2018-07-28 22:37:22 +0300

    Feat: Use proper routing in app

--- src/App.js
+++ src/App.js
@@ -3,25 +3,21 @@ import React from 'react'
 import './App.css'
 import BookList from './BookList';
 import BookSearch from './BookSearch';
+import {Route} from 'react-router-dom';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
   }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <BookSearch/>
-        ) : (
-          <BookList/>)}
+      <Route exact path="/" render={() => (
+        <BookList/>
+      )}/>
+      <Route path="/search" render={() => (
+        <BookSearch/>
+      )}/>
       </div>
     )
   }
