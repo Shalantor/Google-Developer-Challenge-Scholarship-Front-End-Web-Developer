2e241d2984e5b98271638ca963e82b9bd627ad36
George <protipas2@hotmail.com>
2018-07-28 20:17:38 +0300

    Refactor: Change props for book component

--- src/Book.js
+++ src/Book.js
@@ -4,15 +4,15 @@ import MoveMenu from './MoveMenu'
 class Book extends Component {
 
 	render() {
-		console.log(this.props.url)
+		const {book} = this.props;
 		return (
 			<div className="book">
 	          <div className="book-top">
-	            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: this.props.url }}></div>
+	            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.url }}></div>
 	            <MoveMenu/>
 	          </div>
-	          <div className="book-title">{this.props.title}</div>
-	          <div className="book-authors">{this.props.author}</div>
+	          <div className="book-title">{book.title}</div>
+	          <div className="book-authors">{book.author}</div>
 	        </div>
 		)
 	}
