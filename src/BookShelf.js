6ba4af026e9b9986cf9e2cb8b19ffddc30bb5150
George <protipas2@hotmail.com>
2018-07-30 20:33:15 +0300

    Refactor: Remove function from book shelf component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,38 +3,14 @@ import Book from './Book';
 
 class BookShelf extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = props;
-  }
-
-  removeFromShelf = (book,shelf) => {
-
-    //Check if same shelf
-    if (shelf === book.shelf){
-      return;
-    }
-
-    //Remove from shelf
-    this.setState((state) => ({
-      books : state.books.filter((b) => b.title !== book.title)
-    }))
-
-    //Add to new shelf
-    book.setState((state) => ({
-      shelf: shelf
-    }))
-  }
-
 	render() {
-		const {books, title} = this.state;
 		return (
 				<div className="bookshelf">
-                  <h2 className="bookshelf-title">{title}</h2>
+                  <h2 className="bookshelf-title">{this.props.title}</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      {books.map((book =>
-						<Book onRemove = {this.removeFromShelf} book = {book} key={book.title}/>
+                      {this.props.books.map((book =>
+						<Book onRemove = {this.props.changeShelf} book = {book} key={book.title}/>
                       ))}
                     </ol>
                   </div>
