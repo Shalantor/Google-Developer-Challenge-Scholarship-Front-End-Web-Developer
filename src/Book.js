16ffca8c6fea96e27a5f54d7fec9978ac92eff86
George <protipas2@hotmail.com>
2018-07-30 17:03:58 +0300

    Feat: Add state to Book component

--- src/Book.js
+++ src/Book.js
@@ -3,13 +3,18 @@ import MoveMenu from './MoveMenu'
 
 class Book extends Component {
 
+	constructor(props) {
+	    super(props);
+	    this.state = props;
+	}
+
 	render() {
-		const {book} = this.props;
+		const {book, onRemove} = this.state;
 		return (
 			<div className="book">
 	          <div className="book-top">
 	            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.url }}></div>
-	            <MoveMenu/>
+	            <MoveMenu book = {book} onRemove={onRemove}/>
 	          </div>
 	          <div className="book-title">{book.title}</div>
 	          <div className="book-authors">{book.author}</div>
