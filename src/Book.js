2b6873d137b5e92f61e075f975fc939b1692fdd6
George <protipas2@hotmail.com>
2018-07-31 18:01:08 +0300

    Refactor: Change book fields in book component

--- src/Book.js
+++ src/Book.js
@@ -8,11 +8,11 @@ class Book extends Component {
 		return (
 			<div className="book">
 	          <div className="book-top">
-	            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.url }}></div>
+	            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
 	            <MoveMenu book = {book} onRemove={onRemove}/>
 	          </div>
 	          <div className="book-title">{book.title}</div>
-	          <div className="book-authors">{book.author}</div>
+	          <div className="book-authors">{book.authors[0]}</div>
 	        </div>
 		)
 	}
