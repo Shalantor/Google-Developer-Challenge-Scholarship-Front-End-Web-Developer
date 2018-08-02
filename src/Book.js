41a094d2e2003d39b9826c00319658427a161725
George <protipas2@hotmail.com>
2018-08-02 20:10:54 +0300

    Fix: Show books without author or image correctly

--- src/Book.js
+++ src/Book.js
@@ -9,11 +9,11 @@ class Book extends Component {
 			<div className="book">
 	          <div className="book-top">
 	            <div className="book-cover" style={{ width: 128, height: 193, 
-	            	backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
+	            	backgroundImage: `url("${book.imageLinks ? book.imageLinks.smallThumbnail : ''}")` }}></div>
 	            <MoveMenu book = {book} changeShelf={changeShelf}/>
 	          </div>
 	          <div className="book-title">{book.title}</div>
-	          <div className="book-authors">{book.authors[0]}</div>
+	          <div className="book-authors">{book.authors ? book.authors[0] : 'Author not available'}</div>
 	        </div>
 		)
 	}
