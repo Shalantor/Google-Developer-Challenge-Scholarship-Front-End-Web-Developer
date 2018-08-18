bbd2d01118cff0028cf4e2f237b7b88702d81afb
George <protipas2@hotmail.com>
2018-08-18 21:09:22 +0300

    Feat: Implement filtering of locations

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 
 class ListView extends Component {
 
+  state = {
+    input : ''
+  }
+
+  //Update input
+  updateInput = (input) => {
+    this.setState({
+      input : input
+    })
+  }
+
    render() {
 
    return(
@@ -12,12 +23,12 @@ class ListView extends Component {
       <div className="list-view">
         <h1>Locations</h1>
         <div className="filter">
-          <input type="text" placeholder="Filter locations"/>
-          <button><i className="fa fa-filter">Filter</i></button>
+          <input type="text" placeholder="Filter locations" onChange={(event) => this.updateInput(event.target.value)}/>
+          <button onClick={() => this.props.onFilter(this.state.input)}><i className="fa fa-filter">Filter</i></button>
         </div>
         <ul className="list">
           {this.props.markers.map((marker =>
-            <li key = {marker.name} >{marker.location}</li>
+            <li key = {marker.location} >{marker.location}</li>
           ))}
         </ul>
       </div>
